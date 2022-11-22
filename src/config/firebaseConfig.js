8f4a5dd1bc8468289e195be36e1d5f78320f8a57
Errojusidhartha <115140557+Errojusidhartha@users.noreply.github.com>
2022-11-22 17:37:10 +0530

    Add files via upload

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCTynhKsv7nwLe9t6V686g8KgWAVZnjzPQ",
+  authDomain: "crypto-tracker-820f2.firebaseapp.com",
+  projectId: "crypto-tracker-820f2",
+  storageBucket: "crypto-tracker-820f2.appspot.com",
+  messagingSenderId: "28293599644",
+  appId: "1:28293599644:web:dfaadc09158f158bcbf859"
+};
+
+
+
+export default firebaseConfig;
